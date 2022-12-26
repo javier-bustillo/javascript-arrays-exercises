9313af6fdd1869aa9d85d836ad82c44b4abc634b
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-26 12:12:33 +0000

    exercise 8.3 passed

--- exercises/08.3-Sum-all-items/app.js
+++ exercises/08.3-Sum-all-items/app.js
@@ -1,6 +1,8 @@
-function sumTheElements(theArray){
-	let total = 0;
-	//your code here
-
-	return total;
+function sumTheElements(theArray) {
+    let total = 0;
+    for (x of theArray) {
+        total += x;
+    }
+    return total;
 }
+console.log(sumTheElements([1, 2]));
