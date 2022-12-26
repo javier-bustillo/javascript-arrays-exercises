707d402e884950902b4705dfae7af8a83ab6084f
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-26 11:37:02 +0000

    exercise 7.2 passed

--- .learn/resets/07.2-Letter-Counter/app.js
+++ .learn/resets/07.2-Letter-Counter/app.js
@@ -1,19 +1,17 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
-let counts = {};
+let counts = {
 
-// your code here
+};
+
+
 for (let i in par) {
     const letter = par[i].toLowerCase();
-    console.log(letter);
-    if (letter == " ") continue;
-    else if (counts['letter'] == undefined) {
-        console.log("Found " + letter + " for the first time")
-        counts[letter] = 1;
-    } else {
-        console.log("Found " + letter + " more than once")
+    if (counts['letter'] === undefined) {
         counts[letter] = counts[letter] + 1;
-
     }
+
 }
+
+
 
 console.log(counts);
