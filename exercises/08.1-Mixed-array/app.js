ee162a98d2031b5d9098892db8e4752953e0d65e
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-26 11:59:50 +0000

    exercise 8.1 passed

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,8 @@
-let mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
-
-//your code here
+let mix = [42, true, "towel", [2, 1], 'hello', 34.4, {
+    "name": "juan"
+}];
+const myArr = [];
+for (x of mix) {
+    myArr.push(typeof x);
+}
+console.log(myArr)
