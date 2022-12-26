e013d7541493501be0463ea202dee6ddfce51639
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-26 08:55:28 +0000

    exercise 4.2 and 4.3 passed

--- exercises/04.3-Add_items_to_array/app.js
+++ exercises/04.3-Add_items_to_array/app.js
@@ -1,5 +1,6 @@
-let arr = [4,5,734,43,45];
+let arr = [4, 5, 734, 43, 45];
 
-// Your code here, use the push function and the random function.
+arr.push(Math.floor((Math.random() * 100) + 1));
+arr.push(Math.floor((Math.random() * 100) + 1));
 
 console.log(arr);
