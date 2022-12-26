61656828083bbad49dfb05856a3559fc4f4e3dd7
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-26 11:48:59 +0000

    exercise 7.3 passed

--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -1,3 +1,8 @@
-let arr = [45,67,87,23,5,32,60];
+let arr = [45, 67, 87, 23, 5, 32, 60];
+const myArr = [];
+let i = arr.length - 1;
 
-//Your code here.
+for (i; i >= 0; i--) {
+    myArr.push(arr[i]);
+}
+console.log(myArr);
