8637c17c5bdbc7e72f237b88b47850aa87bfda59
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-27 09:41:06 +0000

    exercise 10 passed

--- exercises/10-Everything-is-awesome/app.js
+++ exercises/10-Everything-is-awesome/app.js
@@ -1,9 +1,15 @@
-let myArray = [ 1, 0, 0, 0, 1, 0, 0, 0, 1, 1 ];
+let myArray = [1, 0, 0, 0, 1, 0, 0, 0, 1, 1];
 
 const ZerosToYahoos = (arr) => {
     let return_array = [];
-    arr.forEach((item,index) => {
-            // magic goes inside these brackets
+    arr.forEach(item => {
+        if (item === 1) {
+            return_array.push(item);
+        } else {
+            item = 'Yahoo';
+            return_array.push(item);
+        }
+
     });
     return return_array;
 };
