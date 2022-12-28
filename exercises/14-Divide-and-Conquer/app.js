ccbf52aaae4072cdf18f533b48ddc146c6c7bdbd
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-28 08:58:52 +0000

    exercise 14 passed

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,3 +1,17 @@
-let list_of_numbers = [4, 80, 85, 59, 37,25, 5, 64, 66,	81,20, 64, 41, 22, 76,76, 55, 96, 2, 68];
+let list_of_numbers = [4, 80, 85, 59, 37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76, 55, 96, 2, 68];
 
-// your code here
+const mergeTwoList = (arr) => {
+    const odd = [];
+    const even = [];
+
+    for (x of arr) {
+        if (x % 2 !== 0) {
+            odd.push(x);
+        } else {
+            even.push(x);
+        }
+    }
+    const concatArr = odd.concat(even);
+    return concatArr;
+}
+console.log(mergeTwoList(list_of_numbers));
