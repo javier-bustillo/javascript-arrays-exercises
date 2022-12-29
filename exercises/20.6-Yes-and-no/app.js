be2e884d4d155e5db60bdee1df843f1a8c6ef146
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-29 15:37:45 +0000

    exercise 20.6 passed

--- exercises/20.6-Yes-and-no/app.js
+++ exercises/20.6-Yes-and-no/app.js
@@ -1,3 +1,11 @@
-let theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1];
-
-//your code here
+let theBools = [0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+const myFunction = (val) => {
+    if (val === 1) {
+        val = 'wiki';
+        return val;
+    } else {
+        val = 'woko';
+        return val;
+    }
+};
+console.log(theBools.map(myFunction));
