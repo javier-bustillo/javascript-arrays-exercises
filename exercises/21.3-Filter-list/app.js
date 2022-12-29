0c4ed29537088769c2dfde927027a3d1a0d8bc34
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-29 17:21:31 +0000

    exercise 21.3 passed

--- exercises/21.3-Filter-list/app.js
+++ exercises/21.3-Filter-list/app.js
@@ -1,5 +1,13 @@
-let names = ['Liam','Emma','Noah','Olivia','William','Ava','James','Isabella','Logan','Sophia','Benjamin','Mia','Mason','Charlotte','Elijah','Amelia','Oliver','Evelyn','Jacob','Abigail','Lucas','Harper','Michael','Emily','Alexander','Elizabeth','Ethan','Avery','Daniel','Sofia','Matthew','Ella','Aiden','Madison','Henry','Scarlett','Joseph','Victoria','Jackson','Aria','Samuel','Grace','Sebastian','Chloe','David','Camila','Carter','Penelope','Wyatt','Riley'];
+let names = ['America', 'Liam', 'Emma', 'Noah', 'Olivia', 'William', 'Ava', 'James', 'Isabella', 'Logan', 'Sophia', 'Benjamin', 'Mia', 'Mason', 'Charlotte', 'Elijah', 'Amelia', 'Oliver', 'Evelyn', 'Jacob', 'Abigail', 'Lucas', 'Harper', 'Michael', 'Emily', 'Alexander', 'Elizabeth', 'Ethan', 'Avery', 'Daniel', 'Sofia', 'Matthew', 'Ella', 'Aiden', 'Madison', 'Henry', 'Scarlett', 'Joseph', 'Victoria', 'Jackson', 'Aria', 'Samuel', 'Grace', 'Sebastian', 'Chloe', 'David', 'Camila', 'Carter', 'Penelope', 'Wyatt', 'Riley'];
 
-//declare your function here
+function filterByName(arr, filt) {
+    const newArr = [];
+    for (x of arr) {
+        if (x.includes(filt)) {
+            newArr.push(x);
+        }
+    }
+    return newArr;
+}
 
-console.log(filterByName(names, 'am'));
+console.log(filterByName(names, 'Am'));
