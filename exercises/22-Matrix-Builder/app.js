303fda3cb68f6d5f7d258f7b9709ca44ea560721
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-30 10:11:21 +0000

    exercise 22 passed

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,6 +1,17 @@
-// Code goes here
+function matrixBuilder(val) {
+    let i = 0;
+    let j = 0;
+    let rndNum = [];
+    let arr = [];
 
+    for (j; j < val; j++) {
+        rndNum.push((Math.floor(Math.random() * 2)));
+        arr.push(rndNum);
+    }
+
+    return arr;
+}
 
 
 // do not change anything from this line down
-console.log(matrixBuilder(5))
+console.log(matrixBuilder(2));
