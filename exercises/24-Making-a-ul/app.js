9a1b9b68792bdabbce59b52d2af08e3ef7fff27c
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-01 20:01:34 +0000

    exercise 24 in progress

--- exercises/24-Making-a-ul/app.js
+++ exercises/24-Making-a-ul/app.js
@@ -1,33 +1,68 @@
-let allColors = [
-	{label: 'Red', sexy: true},
-	{label: 'Pink', sexy: false},
-	{label: 'Orange', sexy: true},
-	{label: 'Brown', sexy: false},
-	{label: 'Pink', sexy: true},
-	{label: 'Violet', sexy: true},
-	{label: 'Purple', sexy: false},
+let allColors = [{
+        label: 'Red',
+        sexy: true
+    },
+    {
+        label: 'Pink',
+        sexy: false
+    },
+    {
+        label: 'Orange',
+        sexy: true
+    },
+    {
+        label: 'Brown',
+        sexy: false
+    },
+    {
+        label: 'Pink',
+        sexy: true
+    },
+    {
+        label: 'Violet',
+        sexy: true
+    },
+    {
+        label: 'Purple',
+        sexy: false
+    },
 ];
+/* for (let i in allColors) {
+    if (allColors[i].sexy) {
+        console.log(allColors[i].label);
+    }
+} */
 
-function generateLI(color){
-	// your code here
+function generateLI(color) {
+    htmlLIString = '<li>' +
+        filterColors(color) + '</li>';
+    return htmlLIString;
 }
 
-function filterColors(color){
-	// your code here
+function filterColors(color) {
+    let colors = '';
+    for (let i in allColors) {
+        if (allColors[i].sexy) {
+            console.log(allColors[i].label);
+            return colors += allColors[i].label;
+        }
+    }
+
+
 }
 
-function generateHTMLFromArray(array){
-	
-	let filteredOptions = array.filter(filterColors);
-	let LIs = filteredOptions.map(generateLI);
 
-	let htmlString = '<ul>';
-	LIs.forEach(function(elm){
-		htmlString += elm;
-	})
-	htmlString += '</ul>';
-	return htmlString;
+function generateHTMLFromArray(array) {
+
+    let filteredOptions = array.filter(filterColors);
+    let LIs = filteredOptions.map(generateLI);
+
+    let htmlString = '<ul>';
+    LIs.forEach(function(elm) {
+        htmlString += elm;
+    })
+    htmlString += '</ul>';
+    return htmlString;
 }
 
 console.log(generateHTMLFromArray(allColors));
-
