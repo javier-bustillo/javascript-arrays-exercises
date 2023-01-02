3854a72080235c5b690b4f3c9205b0cd5a1d3ea3
javier-bustillo <jjavier.bustillo@gmail.com>
2023-01-02 20:03:50 +0000

    exercise 25 in progress

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,9 +1,25 @@
-// Add your code here
+function lyricsGenerator(arr) {
+    let lyrics = '';
+    let i = 0;
+    for (i; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            lyrics += ' Boom ';
+        }
+        if ((arr[i] === 1 && arr[i] === (arr[i + 1])) && (arr[i] === arr[i + 2])) {
+            lyrics += ' !!!Break the base!!!';
+        }
+        if (arr[i] === 1) {
+            lyrics += ' Drop the base ';
+        }
+
+    }
+    return lyrics;
+}
 
 
 // Don't change anything bellow this line
-console.log(lyricsGenerator([0,0,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,0]))
-console.log(lyricsGenerator([1,0,1]))
-console.log(lyricsGenerator([1,1,1]))
+console.log(lyricsGenerator([0, 0, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 1, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 0]))
+console.log(lyricsGenerator([1, 0, 1]))
+console.log(lyricsGenerator([1, 1, 1]))
